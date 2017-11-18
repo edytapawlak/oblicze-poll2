9536226e9f91199a4ed57f1dbd29d58f5a8355b1
Edyta Pawlak <edyta.pawlakk@gmail.com>
2017-11-18 15:52:30 +0100

    accomodation cd

--- accomodation/views.py
+++ accomodation/views.py
@@ -6,14 +6,31 @@ from accomodation.models import Dinner, Hostel
 class AccomodationView(TemplateView):
     template_name = 'accomodation/edit_accomodation.html'
     def get(self, request):
-        form_dinner = DinnerForm()
-        form_accomodation = AccomodationForm()
-        
-        # Do zmiany na jakiś mądrzejszy sposób 
+         # Do zmiany na jakiś mądrzejszy sposób 
         dinner_days_list = [ '2011-11-11', '2011-11-12', '2011-11-13' ] 
         hostel_dates_list = [ '2011-11-11', '2011-11-12', '2011-11-13' ]
 
-        args =  {'form_dinner': form_dinner, 'form_accomodation': form_accomodation, 'dinner_dates' : dinner_days_list, 'hostel_dates': hostel_dates_list }
+        user = request.user
+        
+        dinners = Dinner.objects.filter(user = user)
+        checked_din = [str(x.date) for x in dinners]
+        checked_vege = [str(x.date) for x in dinners if x.vege]
+        remark = ''
+        for dinner in dinners:
+            remark += dinner.remark + ' '
+
+        form_dinner = DinnerForm(initial = {'remark': remark})
+
+        nights = Hostel.objects.filter(user = user)
+        checked_night = [str(x.date) for x in nights]
+        print(checked_night)
+
+        form_accomodation = AccomodationForm()
+        args =  {'form_dinner': form_dinner, 'form_accomodation': form_accomodation, 'dinner_dates' : dinner_days_list, 'hostel_dates': hostel_dates_list, 'user_din': checked_din, 'user_vege': checked_vege, 'user_nights': checked_night, }
+
+        for dinner in request.GET.getlist('date'): 
+            print(dinner)
+       
         return render(request, self.template_name, args)
 
     def post(self, request):
@@ -28,14 +45,20 @@ class AccomodationView(TemplateView):
             if form.is_valid():
                 dates =request.POST.getlist('date') 
                 vegeDinner = request.POST.getlist('isVege')
-                for dinner_date in dates:
-                    update_value = {}               
-                    update_value['remark'] = form.cleaned_data['remark']
-                    if (dinner_date in vegeDinner):
-                        update_value['vege'] = True
-                    else: 
-                        update_value['vege'] = False
-                    remark, created = Dinner.objects.update_or_create(user = request.user, date = dinner_date, defaults = update_value)
+                for dinner_date in dinner_days_list:
+                    if dinner_date in dates:
+                        update_value = {}               
+                        update_value['remark'] = form.cleaned_data['remark']
+                        if (dinner_date in vegeDinner):
+                            update_value['vege'] = True
+                        else: 
+                            update_value['vege'] = False
+                        remark, created = Dinner.objects.update_or_create(user = request.user, date = dinner_date, defaults = update_value)
+                    else:
+                        try:
+                            Dinner.objects.get(user = request.user, date = dinner_date).delete()
+                        except Dinner.DoesNotExist:
+                            pass
 
                 hostel_nights = request.POST.getlist('hostel_date')
                 for night in hostel_dates_list:
