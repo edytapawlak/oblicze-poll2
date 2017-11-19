0b9461236ed95ea5f95d57b61e7dc431c3732da2
Edyta Pawlak <edyta.pawlakk@gmail.com>
2017-11-19 23:00:39 +0100

    lectures in admin

--- activities/admin.py
+++ activities/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from activities.models import Poster, Lecture, Author
 
+class LectureAdmin(admin.ModelAdmin):
+    list_display = ('title', 'abstract' )
+
+class PosterAdmin(admin.ModelAdmin):
+    list_display = ('title', 'abstract' )
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author', 'lecture_title', )
+    
+    def author(self, obj):
+        return obj.user.first_name + ' ' + obj.user.last_name
+
+    def lecture_title(self, obj):
+        return obj.lecture_id.title
+    
+    def poster_title(self, obj):
+        return obj.poster_id.title
+
 # Register your models here.
-admin.site.register(Lecture)
-admin.site.register(Poster)
-admin.site.register(Author)
+admin.site.register(Lecture, LectureAdmin)
+admin.site.register(Poster, PosterAdmin)
+admin.site.register(Author, AuthorAdmin)
