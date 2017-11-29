9d30f2bcacab376c3d5eac0472631198b4850808
Edyta Pawlak <edyta.pawlakk@gmail.com>
2017-11-29 11:02:14 +0100

    hosts

--- oblicze/settings.py
+++ oblicze/settings.py
@@ -20,7 +20,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-ALLOWED_HOSTS = [ 'oblicze.herokuapp.com' ]
+ALLOWED_HOSTS = [ 'calm-citadel-11411.herokuapp.com' ]
 
 
 # Application definition
