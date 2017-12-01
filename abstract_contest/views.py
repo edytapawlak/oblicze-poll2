40fa7b3defb971c70b9b1938ec1ba605f844afb4
Edyta Pawlak <edyta.pawlakk@gmail.com>
2017-12-01 21:41:27 +0100

    konkurs referatów

--- abstract_contest/views.py
+++ abstract_contest/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
 from activities.models import Lecture, Status 
 from abstract_contest.models import ChoosedLecture
 
+from django.contrib.auth.decorators import user_passes_test
+
+def group_required(*group_names):
+    """Requires user membership in at least one of the groups passed in."""
+    def in_groups(u):
+        if u.is_authenticated():
+            if bool(u.groups.filter(name__in=group_names)) | u.is_superuser:
+                return True
+        return False
+    return user_passes_test(in_groups)
+
 # Create your views here.
+@group_required('admins','Komisja')
 def choose_lectures(request):
     if (request.method == "POST"):
         print(request.POST.getlist('correct'))
