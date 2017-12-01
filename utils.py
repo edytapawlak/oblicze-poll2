b01d8e348cae07651b1ec6324fc99c4c4a7007f4
Edyta Pawlak <edyta.pawlakk@gmail.com>
2017-12-01 13:37:55 +0100

    troche porządku

--- utils.py
+++ utils.py
@@ -1,19 +1,23 @@
 import os
 import subprocess
-from tex_templates.tex_const import PREAMBULE, END
+from tex_templates.tex_const import PREAMBULE, END, TMP_OUTPUT
 
 def is_tex_valid(text):
-    with open('tex_templates/cover.tex','w+') as f:
+    with open(TMP_OUTPUT + 'tmp.tex','w+') as f:
         f.write(PREAMBULE + '\n' + text + END)
 
-    cmd = ['pdflatex', '-interaction', 'nonstopmode', 'tex_templates/cover.tex']
+    cmd = ['pdflatex', '-interaction', 'nonstopmode', TMP_OUTPUT + 'tmp.tex']
     proc = subprocess.Popen(cmd)
     proc.communicate()
 
     retcode = proc.returncode
-    os.unlink('tex_templates/cover.tex')
-    os.unlink('cover.log')
-    os.unlink('cover.aux')
+    os.unlink( TMP_OUTPUT + 'tmp.tex')
+    try:
+        os.unlink('tmp.pdf')
+    except OSError:
+        pass
+    os.unlink('tmp.log')
+    os.unlink('tmp.aux')
 
     if not retcode == 0:
         raise ValueError('Error {} executing command: {}'.format(retcode, ' '.join(cmd))) 
